e024e26d398628bf95aee3145bb788d0a14a416c
Julio Cesar de Jesus <cesar.juliojc007@gmail.com>
2022-06-10 10:34:45 -0300

    Classe Produto atualizado

--- lib/Poduto.dart
+++ lib/Poduto.dart
@@ -6,7 +6,7 @@ class Produtos {
       imagem: 'imagens/imagem1.jpg',
       marca: 'Olympikus',
       nome: 'Fila Recovery Corrida',
-      valor: 150,
+      valor: 150.00,
       corDoProduto: 'Preto',
       descricao:
           'O Recovery Corrida é ideal para a prática de exercícios físicos, como corridas e TFM, formato anatômico, muito leve, possui o tecido dupla frontura para transpirar o suor e ventilar o pé.',
@@ -16,7 +16,7 @@ class Produtos {
       imagem: 'imagens/imagem2.jpg',
       marca: 'Nike',
       nome: 'Downshifter 10 - Feminino Corrida',
-      valor: 350,
+      valor: 350.00,
       corDoProduto: 'Banco',
       descricao:
           'Desde uma tranquila caminhada aos treinos fitness e corrida leve faz do Tênis Nike Downshifter 10 uma ótima opção',
@@ -26,7 +26,7 @@ class Produtos {
       imagem: 'imagens/imagem3.jpg',
       marca: 'Adiddas',
       nome: 'Lite Racer 2.0 feminino',
-      valor: 300,
+      valor: 300.00,
       corDoProduto: 'Preto',
       descricao:
           'Use e abuse da versatilidade e estilo do Tênis Lite Racer 3.0 Feminino para compor o seu visual urbano',
@@ -36,7 +36,7 @@ class Produtos {
       imagem: 'imagens/imagem4.jpg',
       marca: 'Puma',
       nome: 'Flyer Runner Feminino Corrida',
-      valor: 250,
+      valor: 250.00,
       corDoProduto: 'Cinza',
       descricao:
           'Alcance altas performances com o tênis Puma flyer runner, seu cabedal é confeccionado em material têxtil macio',
@@ -46,7 +46,7 @@ class Produtos {
       imagem: 'imagens/imagem5.jpg',
       marca: 'Vans',
       nome: 'UltraRange Exo 5',
-      valor: 200,
+      valor: 200.00,
       corDoProduto: 'Preto',
       descricao:
           'O novo membro da família UltraRange chegou com muito estilo e autenticidade, atualização versátil e desenvolvida para entregar conforto, suporte, grip e durabilidade.',
@@ -56,10 +56,29 @@ class Produtos {
       imagem: 'imagens/imagem6.jpg',
       marca: 'Fila',
       nome: 'Recovery Corrida - Tênis Masculino',
-      valor: 250,
+      valor: 250.99,
       corDoProduto: 'Branco',
       descricao:
           'Com design moderno e único, o Tênis Fila Recovery Masculino chama a atenção pela sua beleza e conforto. Esse tênis para correr é confeccionado com material respirável,',
+      tempoDeEntrega: ' Prazo de entrega 7 dias uteis',
+    ),
+    Item(
+      imagem: 'imagens/imagem7.jpg',
+      marca: 'Mizuno',
+      nome: 'Mirai Original Corrida Esportivo',
+      valor: 200.00,
+      corDoProduto: 'Branco',
+      descricao:
+          'Tênis Mizuno Mirai 3 é uma opção perfeita para treinos e academia, devido seu cabedal todo confeccionado em material respirável, leve e resistente.',
+    ),
+    Item(
+      imagem: 'imagens/imagem8.jpg',
+      marca: 'Reebuk',
+      nome: 'Nano X - Tênis Masculino',
+      valor: 375.99,
+      corDoProduto: 'Branco',
+      descricao:
+          'Um tênis para corridas e caminhadas leves e próprio para academia.',
       tempoDeEntrega: ' Prazo de entrega 7 dias uteis',
     ),
   ];
